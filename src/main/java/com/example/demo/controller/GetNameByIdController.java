f73e52fe496f7a9c25262b347c19a1f05280f006
zuziming <957349728@qq.com>
2021-09-18 18:07:23 +0800

    放â//£抙

--- src/main/java/com/example/demo/controller/GetNameByIdController.java
+++ src/main/java/com/example/demo/controller/GetNameByIdController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/name")
 public class GetNameByIdController {
+    @GetMapping("/list")
     public String getNameById(@RequestParam Integer id){
         return "yourName";
     }
